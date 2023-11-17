30dd1ad53272877340408bc376d64a18ffd63123
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-11-17 12:50:01 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,38 +12,34 @@ topiclist = [key for key in ethibot_topics.topics]
 # Create interface
 st.title("💬 Talk to EthiBot") 
 if "messages" not in st.session_state:
-    with st.sidebar:
-        topic = st.selectbox('What would you like to discuss about?', topiclist)
-        persona = ethibot_topics.topics[topic]['persona']
-        initial_message = ethibot_topics.topics[topic]['initial message']
-        if st.button("Let's start!", type="primary"):
-            st.session_state["messages"] = [{"role": "system", "content": persona}, {"role": "assistant", "content": initial_message}]
+    topic = st.selectbox('What would you like to discuss about?', topiclist)
+    persona = ethibot_topics.topics[topic]['persona']
+    initial_message = ethibot_topics.topics[topic]['initial message']
+    if st.button("Let's start!", type="primary"):
+        st.session_state["messages"] = [{"role": "system", "content": persona}, {"role": "assistant", "content": initial_message}]
+else:
+    # Define hidden instructions
+    prompt_instruction = 'Reflect on the system role before answering. Try to behave as much as possible as the entity described in your system role. Check thoroughly your output against the system role.'
     
-   
-            # Define hidden instructions
-            prompt_instruction = 'Reflect on the system role before answering. Try to behave as much as possible as the entity described in your system role. Check thoroughly your output against the system role.'
-            
-            
-            # Show messages
-            
-            for msg in st.session_state.messages:
-                if msg['role'] != 'system':
-                    st.chat_message(msg['role']).write(msg['content'])
-                if msg['role'] == 'assistant':
-                    msg['content'] = msg['content'].replace(prompt_instruction, '')
-            
-            
-            # Logic
-            if prompt := st.chat_input():
-                openai.api_key = openai_api_key
-                prompt_send = prompt_instruction + ' ' + prompt
-                st.session_state.messages.append({"role": "user", "content": prompt})
-                st.chat_message("user").write(prompt)
-                response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-                
-                msg_role = response.choices[0].message.role
-                msg_content = response.choices[0].message.content
-            
-                msg = {'role' : msg_role, 'content' : msg_content}
-                st.session_state.messages.append(msg)
-                st.chat_message("assistant").write(msg_content)
+    # Show messages
+    for msg in st.session_state.messages:
+        if msg['role'] != 'system':
+            st.chat_message(msg['role']).write(msg['content'])
+        if msg['role'] == 'assistant':
+            msg['content'] = msg['content'].replace(prompt_instruction, '')
+    
+    
+    # Logic
+    if prompt := st.chat_input():
+        openai.api_key = openai_api_key
+        prompt_send = prompt_instruction + ' ' + prompt
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+        
+        msg_role = response.choices[0].message.role
+        msg_content = response.choices[0].message.content
+    
+        msg = {'role' : msg_role, 'content' : msg_content}
+        st.session_state.messages.append(msg)
+        st.chat_message("assistant").write(msg_content)
