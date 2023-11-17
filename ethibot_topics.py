1c2a56b270e049e37f87820a736bc1ff1a231b56
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-11-17 12:18:49 +0100

    Update ethibot_topics.py

--- ethibot_topics.py
+++ ethibot_topics.py
@@ -1,4 +1,4 @@
-topics = {'assisted suicide' : {'name' : 'assisted suicide', 'persona' : 'this bio'}, 
-          'surrogacy' : {'name' : 'surrogacy', 'persona' : 'another bio'},
-          'organ donation' : {'name' : 'organ donation', 'persona' : 'a third bio'},           
+topics = {'assisted suicide' : {'name' : 'assisted suicide', 'persona' : 'this bio', 'initial message' : 'this is the initial message'}, 
+          'surrogacy' : {'name' : 'surrogacy', 'persona' : 'another bio', 'initial message' : 'this is the initial message'},
+          'organ donation' : {'name' : 'organ donation', 'persona' : 'a third bio', 'initial message' : 'this is the initial message'},           
           }
