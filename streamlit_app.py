5db8b6d9fd668b2359c5a882bd3eee47227dbcc6
that-ugly-cat <98877284+that-ugly-cat@users.noreply.github.com>
2023-11-17 12:47:04 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,30 +20,30 @@ if "messages" not in st.session_state:
             st.session_state["messages"] = [{"role": "system", "content": persona}, {"role": "assistant", "content": initial_message}]
     
    
-# Define hidden instructions
-prompt_instruction = 'Reflect on the system role before answering. Try to behave as much as possible as the entity described in your system role. Check thoroughly your output against the system role.'
-
-
-# Show messages
-
-for msg in st.session_state.messages:
-    if msg['role'] != 'system':
-        st.chat_message(msg['role']).write(msg['content'])
-    if msg['role'] == 'assistant':
-        msg['content'] = msg['content'].replace(prompt_instruction, '')
-
-
-# Logic
-if prompt := st.chat_input():
-    openai.api_key = openai_api_key
-    prompt_send = prompt_instruction + ' ' + prompt
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
-    
-    msg_role = response.choices[0].message.role
-    msg_content = response.choices[0].message.content
-
-    msg = {'role' : msg_role, 'content' : msg_content}
-    st.session_state.messages.append(msg)
-    st.chat_message("assistant").write(msg_content)
+            # Define hidden instructions
+            prompt_instruction = 'Reflect on the system role before answering. Try to behave as much as possible as the entity described in your system role. Check thoroughly your output against the system role.'
+            
+            
+            # Show messages
+            
+            for msg in st.session_state.messages:
+                if msg['role'] != 'system':
+                    st.chat_message(msg['role']).write(msg['content'])
+                if msg['role'] == 'assistant':
+                    msg['content'] = msg['content'].replace(prompt_instruction, '')
+            
+            
+            # Logic
+            if prompt := st.chat_input():
+                openai.api_key = openai_api_key
+                prompt_send = prompt_instruction + ' ' + prompt
+                st.session_state.messages.append({"role": "user", "content": prompt})
+                st.chat_message("user").write(prompt)
+                response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
+                
+                msg_role = response.choices[0].message.role
+                msg_content = response.choices[0].message.content
+            
+                msg = {'role' : msg_role, 'content' : msg_content}
+                st.session_state.messages.append(msg)
+                st.chat_message("assistant").write(msg_content)
